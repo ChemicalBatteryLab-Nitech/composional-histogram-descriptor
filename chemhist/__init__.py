403746deb123183fb50537f87a6c7c1186799953
Masanobu NAKAYAMA <102635361+Masanobu13@users.noreply.github.com>
2025-11-03 21:58:26 +0900

    Add files via upload

---
+++ chemhist/__init__.py
@@ -0,0 +1,10 @@
+# chemhist/__init__.py
+from .core import get_descriptor, compconv, dfmake, broad
+
+__all__ = [
+    "get_descriptor",
+    "compconv",
+    "dfmake",
+    "broad",
+    "readdefelem"
+]
